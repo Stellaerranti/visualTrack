f7bd1fc843e411b9b3867c6098b7eb5d66b599d5
Stellaerranti <ushakovda@my.msu.ru>
2024-03-16 21:12:00 +0300

    Data read
    Реализовано считывание из файла

--- VisualTrack/VisualTrack/Form1.Designer.cs
+++ VisualTrack/VisualTrack/Form1.Designer.cs
@@ -31,7 +31,21 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Zeta = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.zetaTable = new System.Windows.Forms.DataGridView();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.U = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.U_std = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ca_std = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Trs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.S = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ImportZeta = new System.Windows.Forms.OpenFileDialog();
             this.tabControl1.SuspendLayout();
+            this.Zeta.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zetaTable)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,6 +61,7 @@
             // 
             // Zeta
             // 
+            this.Zeta.Controls.Add(this.tableLayoutPanel1);
             this.Zeta.Location = new System.Drawing.Point(4, 22);
             this.Zeta.Name = "Zeta";
             this.Zeta.Padding = new System.Windows.Forms.Padding(3);
@@ -65,6 +80,93 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.zetaTable, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(786, 418);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // zetaTable
+            // 
+            this.zetaTable.AllowUserToAddRows = false;
+            this.zetaTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.zetaTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.U,
+            this.U_std,
+            this.Ca,
+            this.Ca_std,
+            this.Trs,
+            this.S});
+            this.zetaTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zetaTable.Location = new System.Drawing.Point(3, 3);
+            this.zetaTable.Name = "zetaTable";
+            this.zetaTable.RowHeadersVisible = false;
+            this.zetaTable.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.zetaTable.Size = new System.Drawing.Size(583, 412);
+            this.zetaTable.TabIndex = 0;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.Name = "NameColumn";
+            // 
+            // U
+            // 
+            this.U.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.U.HeaderText = "U";
+            this.U.Name = "U";
+            // 
+            // U_std
+            // 
+            this.U_std.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.U_std.HeaderText = "U std";
+            this.U_std.Name = "U_std";
+            // 
+            // Ca
+            // 
+            this.Ca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Ca.HeaderText = "Ca";
+            this.Ca.Name = "Ca";
+            // 
+            // Ca_std
+            // 
+            this.Ca_std.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Ca_std.HeaderText = "Ca std";
+            this.Ca_std.Name = "Ca_std";
+            // 
+            // Trs
+            // 
+            this.Trs.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Trs.HeaderText = "Trs";
+            this.Trs.Name = "Trs";
+            // 
+            // S
+            // 
+            this.S.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.S.HeaderText = "S";
+            this.S.Name = "S";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -72,8 +174,11 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " VisualTrack";
             this.tabControl1.ResumeLayout(false);
+            this.Zeta.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.zetaTable)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +188,17 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage Zeta;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView zetaTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn U;
+        private System.Windows.Forms.DataGridViewTextBoxColumn U_std;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ca_std;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Trs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn S;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ImportZeta;
     }
 }
 
--- VisualTrack/VisualTrack/Form1.cs
+++ VisualTrack/VisualTrack/Form1.cs
@@ -16,5 +16,46 @@ namespace VisualTrack
         {
             InitializeComponent();
         }
+
+        private void readFile()
+        {
+            if (ImportZeta.ShowDialog() == DialogResult.Cancel) { return; }
+
+            string filename = ImportZeta.FileName;
+            int ni = 0;
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(filename);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    ni = i;
+
+                    if (lines[i].Length > 2)
+                    {
+                        while (lines[i].Contains("  ")) { lines[i] = lines[i].Replace("  ", " "); }
+                        while (lines[i].Contains("\t\t")) { lines[i] = lines[i].Replace("\t\t", "\t"); }
+                        while (lines[i].Contains("\t ")) { lines[i] = lines[i].Replace("\t ", "\t"); }
+                        while (lines[i].Contains(" \t")) { lines[i] = lines[i].Replace(" \t", "\t"); }
+                        while (lines[i].Contains(".")) { lines[i] = lines[i].Replace(".", ","); }
+
+                        string[] line = lines[i].Split(new[] { ' ', '\t' });
+
+                        zetaTable.Rows.Add(line[0], line[1], line[2], line[3],
+                            line[4], line[5], line[6]);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error while reading file at line " + (ni + 1).ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readFile();
+        }
     }
 }
